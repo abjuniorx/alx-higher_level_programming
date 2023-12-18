23587d275da40f29973acc5ff22e1e47c6909308
abjuniorx <abdurahman856@gmail>
2023-12-18 12:25:28 -0800

    python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     num = 0
     for i in range(x):
@@ -9,4 +10,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     Return
-
